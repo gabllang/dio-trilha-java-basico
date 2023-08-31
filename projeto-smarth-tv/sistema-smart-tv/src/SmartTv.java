86061285b6c1216c684afe55a69ab3542a1df1a9
Gabriel <davgabie@gmail.com>
2023-08-31 10:17:26 -0300

    aula metodos (smart tv)

---
+++ projeto-smarth-tv/sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,37 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+        System.out.println("seu canal é: " + canal);
+    }
+
+    public void diminuirCanal(){
+        canal--;
+        System.out.println("seu canal é: " + canal);
+    }
+    
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("aumentando o voolume para: " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("diminuindo o voolume para: " + volume);
+    }
+
+    public void ligar(){
+        ligada = true;
+    }
+    
+    public void desligar(){
+        ligada = false;
+    }
+}
