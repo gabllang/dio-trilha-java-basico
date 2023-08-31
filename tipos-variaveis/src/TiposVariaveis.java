d5feeeaada09ce572e3ebfaf78cc1c5d2412b0f6
Gabriel <davgabie@gmail.com>
2023-08-31 01:17:52 -0300

    exercicio tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1; 
+        int numeroNormal = numeroCurto;
+
+        short numeroCurto2 = (short)numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
+        // final deixa a var constante (não pode ser mudada)
+        // por convenção, as constantes devem ser escritas em caixa alta separadas por underline 
+
+        System.out.println(VALOR_DE_PI);
     }
 }
