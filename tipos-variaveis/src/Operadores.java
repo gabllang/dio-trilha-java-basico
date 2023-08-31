81de1db6f69c769f0c63b69e28a48560f466292f
Gabriel <davgabie@gmail.com>
2023-08-31 08:55:05 -0300

    operadores atualização

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -38,6 +38,44 @@ public class Operadores {
         concatenacao = "1"+(1+1+1);
 
         System.out.println(concatenacao);
+
+
+        int numero = 5;
+
+        // não guarda o valor de negativo
+        System.out.println(- numero);
+        System.out.println(numero);
+
+        numero = 5;
+        // forma correta de deixar um numero negativo
+        numero = - numero;
+
+        System.out.println(numero);
+
+        // deixando positivo
+        numero = + numero;
+        
+        System.out.println(numero);
+        // ops... o que deu errado?
+        // dessa forma iria "somar" deixando (-) + (+) = (-)
+        // sinais diferentes = - sinais iguais = +
+        // então seguindo a logica da matematica 
+        // deixamos um numero positivo assim
+
+        numero = - numero;
+        // (-) + (-) = (+)
+
+        System.out.println(numero);
+
+        // tinha feito isso tudo de cabeça, o cara da aula da dio fez assim
+        // ele multiplicou por -1, obviamente funciona, porque ele não ensinou da forma que eu fiz?
+        // será que como eu fiz está errado???
+
+        // só explicando, qualquer numero multiplicado por 1 da ele mesmo, e fez a regrinha lá dos sinais inversos
+        numero = numero * -1;
+
+        System.out.println(numero);
+
     }
 
 }
