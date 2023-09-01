acc8d234f7ce156a868c6506c1075ba4b83821bc
Gabriel <davgabie@gmail.com>
2023-09-01 09:24:48 -0300

    setembro

---
+++ estruturas-e-condicionais/controle-fluxo/src/AboutMe.java
@@ -0,0 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+
+        // try, eu acho que é um campo que serve para mandar informações para o catch
+
+        try{ 
+
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close();   
+        }
+
+        // catch serve para "pegar o erro" e fazer algo com ele
+        catch(InputMismatchException e) {
+            System.err.println("Os campos idade e altura precisem ser numericos");
+        }
+    }
+}
---
+++ estruturas-e-condicionais/controle-fluxo/src/FormatadorDeCepExemplo.java
@@ -0,0 +1,19 @@
+// ENTENDI NADA, CARACA, PRECISO ESTUDAR ISSOr
+
+public class FormatadorDeCepExemplo {
+     public static void main(String[] args) {
+        String cetry {
+            p
+        } catch (Exception e) {
+            // TODO: handle exception
+        }Formatado = formatarCep ("23765064");
+     }
+
+     static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+}
